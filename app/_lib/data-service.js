f8a6fddeb07982ddc5dbeed2589ba74d76700eee
Rafael Gerotto <rafael.gerotto@hotmail.com>
2025-01-19 20:41:56 +0100

    implementing suspense on the cabin page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -40,6 +40,8 @@ export const getCabins = async function () {
     .from('cabins')
     .select('id, name, maxCapacity, regularPrice, discount, image')
     .order('name');
+      // For testing
+   //await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
