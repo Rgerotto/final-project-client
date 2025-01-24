ef6758e5902f473043711c269fc14b3117b4b76e
Rafael Gerotto <rafael.gerotto@hotmail.com>
2025-01-24 23:07:22 +0100

    client components render into a server

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,7 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import TextExpander from "@/app/_components/TextExpander";
 
 export async function generateMetadata({params}){
 const {name} = await getCabin(params.cabinId);
@@ -8,6 +9,14 @@ return {
     title: `Cabin ${name}`
 }
 }
+
+export async function generateStaticParams(){
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({cabinId: String(cabin.id)}));
+  return ids;
+}
+
 export default async function Page({params}) {
     const cabin = await getCabin(params.cabinId)
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
@@ -25,7 +34,11 @@ export default async function Page({params}) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>
+            {description}
+            </TextExpander>
+            </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: 'Cabins'
 }
-export default function Page() {
+export default function Page({searchParams}) {
+//console.log(searchParams);
 
+const filter = searchParams?.capacity ?? 'all';
 
   return (
     <div>
@@ -21,8 +24,11 @@ export default function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-      <Suspense fallback={<Spinner/>}>
-      <CabinList />
+      <div className="flex justify-end mb-8">
+      <Filter/>
+      </div>
+      <Suspense fallback={<Spinner/>} key={filter}>
+      <CabinList filter={filter} />
       </Suspense>
     </div>
   );
