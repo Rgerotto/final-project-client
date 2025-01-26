f14e95bee612287aac109cb3a91fb4527620c23d
Rafael Gerotto <rafael.gerotto@hotmail.com>
2025-01-26 23:01:32 +0100

    including calendar and form

---
+++ app/_components/Reservation.js
@@ -0,0 +1,21 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector"
+import ReservationForm from "./ReservationForm"
+
+async function Reservation({cabin}) {
+    const [settings, bookedDates] = await Promise.all([
+        getSettings(),
+        getBookedDatesByCabinId(cabin.id),
+      ]);
+    return (
+        <div>
+        
+        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px">
+          <DateSelector settings={settings} bookedDate={bookedDates} cabin={cabin}/>
+          <ReservationForm cabin={cabin} />
+        </div>
+      </div>
+    )
+}
+
+export default Reservation
